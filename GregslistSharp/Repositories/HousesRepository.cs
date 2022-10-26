fd6ad67d126325a20de9a97cdddf080d66655466
Coel Allen <coelallen@gmail.com>
2022-10-26 14:18:39 -0600

    first changes

---
+++ GregslistSharp/Repositories/HousesRepository.cs
@@ -0,0 +1,57 @@
+namespace GregslistSharp.Repositories;
+
+public class HousesRepository
+{
+  private readonly IDbConnection _db;
+
+  public HousesRepository(IDbConnection db)
+  {
+    _db = db;
+  }
+  public List<House> GetHouses()
+  {
+    var sql = "SELECT * FROM houses";
+    return _db.Query<House>(sql).ToList();
+  }
+
+  public House CreateHouse(House houseData)
+  {
+    var sql = @"INSERT INTO 
+    houses(
+      bedrooms, floors, year, description, price, imgUrl
+    )
+    VALUES(
+      @Bedrooms, @Floors, @Year, @Description, @Price, @ImgUrl
+    );
+    SELECT LAST_INSERT_ID();
+    ";
+    houseData.Id = _db.ExecuteScalar<int>(sql, houseData);
+    return houseData;
+  }
+
+  public House EditHouse(House houseData)
+  {
+    var sql = @"
+   UPDATE houses SET
+      bedrooms = @Bedrooms,
+      floors = @Floors,
+      year = @Year, 
+      description = @Description,
+      price = @Price,
+      imgUrl = @ImgUrl
+    WHERE id = @Id;";
+    // NOTE Commas before wheres are bad 
+    var rowsAffected = _db.Execute(sql, houseData);
+    if (rowsAffected == 0)
+    {
+      throw new Exception("Unable to Edit");
+    }
+    return houseData;
+  }
+
+  public House GetHouse(int id)
+  {
+    var sql = "SELECT * FROM houses WHERE Id =id";
+    return _db.Query<House>(sql).FirstOrDefault();
+  }
+}
